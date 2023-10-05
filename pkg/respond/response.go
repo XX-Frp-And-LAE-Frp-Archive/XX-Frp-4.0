889ca9d7cd5b4a46ed6f854f55c1664d48df8cad
Aehxy <ahmrcxy@gmail.com>
2023-10-05 20:59:49 +0800

    修改为status防止Frps不适配

--- pkg/respond/response.go
+++ pkg/respond/response.go
@@ -14,7 +14,7 @@ type Response struct {
 // Response 返回的 API 响应
 func Respond(c *gin.Context, code int, message string, data interface{}) {
 	res := gin.H{
-		"code":    code,
+		"status":    code,
 		"message": message,
 	}
 
