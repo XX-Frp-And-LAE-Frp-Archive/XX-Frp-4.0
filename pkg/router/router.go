466ea587085e0919e2a2415868adbaf154a94ba4
xyyy <79127140+ahmr-bot@users.noreply.github.com>
2026-07-31 02:21:17 +0800

    Update NoRoute response message in router.go

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -130,6 +130,6 @@ func LoadRoutes(r *gin.Engine, db *gorm.DB) {
 
 	// 监听默认路由
 	r.NoRoute(func(c *gin.Context) {
-		c.JSON(404, gin.H{"code": 404, "message": "ME Frp 4.0 API Server Is OK!"})
+		c.JSON(404, gin.H{"code": 404, "message": "XX Frp 4.0 API Server Is OK!"})
 	})
 }
